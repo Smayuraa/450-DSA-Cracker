7f3456668869b4a34217db56f6024a7cace7be05
DSA <mayurisharma2003@gmail.com>
2025-05-09 13:48:24 +0530

    revArrSwap

---
+++ Array/Reverse_Array_Swap.java
@@ -0,0 +1,25 @@
+public class Reverse_Array_Swap {
+     static void RevArrSwapFun(int arr[])
+     {
+        int left=0,right=arr.length-1;
+
+        while(left<right){
+            int temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+
+            left++;--right;
+        }
+     }
+     public static void main(String[]args)
+     {
+        int arr[]={1,2,3,4};
+        RevArrSwapFun(arr);
+     
+
+     for(int i=0;i<arr.length;i++)
+     {
+        System.out.println(arr[i]+" ");
+     }
+    }
+}
